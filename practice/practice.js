1a1bc7b20b1c68bcb26dc19b7dd508dafa527546
camp user <camp@ma-15-camp-1.stlukesct.org>
2019-07-11 16:32:31 -0400

    i completed if else statements

--- practice/practice.js
+++ practice/practice.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
   // your code goes below
-
-
+console.log("the page is now loaded!");
+var givenNumber = prompt("give me a number");
+console.log(typeof givenNumber);
+var numAsint = parseInt(givenNumber);
+console.log(numAsint);
+console.log(typeof numAsint)
+if  ( numAsint <4){
+	console.log("this number is less than four");
+}else{
+	console.log("something went wrong")
+}
 
 })
--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() { 
   // write your javascript code below.
-  
+//   var age = prompt("how old are you")
+// if  ( age <16){
+// 	alert("YOU CANT DRIVE");
+// }else{
+// 	alert("HERE'S THE KEYS TO MY FERRARI!")
+// }
+// var birthday = prompt ("is it your birthday?")
+// console.log (birthday)
+// if (birthday == "yes"){
+// 	alert( "Happy Birthday!")
+// }else{
+// 	alert(good day mate!"")
+// }
 
-
-
-
-
-
+// if birthday = alert ("Happy Birthday!")
+var favoriteColor = prompt ("what's your favorite color")
+if (favoriteColor == "red"){
+ alert("red is also my favorite color")
+ }else if (favoriteColor == "blue"){
+ 	alert("blue is a really good color")
+ }else {
+ 	alert("that color is not very cash money")
+}
 
 
 
