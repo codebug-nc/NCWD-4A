ca020eb6688d9a868b66f400f9f1c65fe5d0d562
Alex Chadwick <alexchadwick@Alexs-MacBook-Pro-7.local>
2019-07-11 16:29:45 -0400

    gg

--- practice/practice.js
+++ practice/practice.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // your code goes below
-
-
-
+	console.log("the page has loaded");
+	var givenNumber=prompt("Give me a number");
+	console.log(givenNumber);
+	console.log(typeof givenNumber);
+	var number=parseInt(givenNumber);
+	console.log(number);
+	console.log(typeof number);
+	if(number<4){
+		console.log("Yasss");
+	} else if (number >=4) {
+		console.log("this number is NOT less than four!");
+	}
+	else{
+		console.log("You screwed up")
+	}
 })
--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() { 
-  // write your javascript code below.
-  
-
-
-
-
-
-
-
-
-
-
-
-
-  
+console.log ("the page has loaded")
+// 	var age= prompt ("how old are you")
+// 	console.log (age)
+// 	if (age<16) {
+// 		alert ("You can't drive")
+// 	}
+// 	else {
+// 		alert ("HERE'S THE KEY TO MY FERRARI")
+// 	}
+// 	var birthdayIsToday=prompt ("is it your birthday");
+// 	if (birthdayIsToday=="yes")
+// 	{
+// 		alert ("HAPPY BIRTHDAY!!")
+// 	}
+// 	else (birthdayIsToday=="no"){
+// 		alert ("good day mate");
+// 	}
+console.log prompt ("are you hungry")
 });
